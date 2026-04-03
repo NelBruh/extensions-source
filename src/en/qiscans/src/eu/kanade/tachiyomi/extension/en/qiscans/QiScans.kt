afb3d6ec4d1c157f6c886094844708d48e096fef
NelBruh <nelbruh05@gmail.com>
2026-04-03 23:41:37 +0200

    new src code

--- src/en/qiscans/src/eu/kanade/tachiyomi/extension/en/qiscans/QiScans.kt
+++ src/en/qiscans/src/eu/kanade/tachiyomi/extension/en/qiscans/QiScans.kt
@@ -21,12 +21,41 @@ class QiScans : HttpSource() {
     val apiUrl: String = "https://api.qimanhwa.com/api/v1"
     override val supportsLatest = true
 
-    override val client = super.client.newBuilder()
+    override fun headersBuilder() = super.headersBuilder().apply {
+        add("Accept", "application/json, text/plain, */*")
+        add("Origin", "https://qimanhwa.com")
+        add("Referer", "https://qimanhwa.com/")
+        add("Sec-Fetch-Dest", "empty")
+        add("Sec-Fetch-Mode", "cors")
+        add("Sec-Fetch-Site", "same-site")
+    }
+
+
+    override val client = network.cloudflareClient.newBuilder()
         .rateLimit(3, 1, TimeUnit.SECONDS)
         .build()
 
+
+    override fun chapterListRequest(manga: SManga): Request {
+        val url = "$baseUrl/series/${manga.url}"
+        return GET(url, headers)
+    }
     override fun chapterListParse(response: Response): List<SChapter> {
-        TODO("Not yet implemented")
+        val body = response.body.string()
+        val slug = response.request.url.pathSegments.last()
+        var chapterPage = 1
+        val cList = mutableListOf<Chapter>()
+        do {
+            val chapterUrl = "$apiUrl/series/$slug/chapters".toHttpUrl().newBuilder().apply {
+                addQueryParameter("page", chapterPage.toString())
+                addQueryParameter("perPage", "30")
+                addQueryParameter("sort", "desc")
+            }.build()
+            var res = client.newCall(GET(chapterUrl, headers)).execute().parseAs<ChapterList>()
+            cList.addAll(res.data)
+            chapterPage++
+        } while (res.nextPage != null)
+        return cList.toList().map { it.toSChapter(slug) }
     }
 
     override fun imageUrlParse(response: Response): String {
@@ -47,19 +76,28 @@ class QiScans : HttpSource() {
     override fun latestUpdatesParse(response: Response): MangasPage {
         val res = response.parseAs<SearchResponse>()
         var page = res.currentPage
-        var hasNext = !res.nextPage.equals(null)
+        var hasNext = !res.nextPage!!.equals(null)
         val entries = res.data.filterNot { it.type == "NOVEL" }.map { it.toSManga() }
         return MangasPage(entries, hasNext)
     }
 
-    override fun mangaDetailsParse(response: Response): SManga {
-        TODO("Not yet implemented")
-    }
+    // =============================== Details ===============================
+    override fun getMangaUrl(manga: SManga): String = "$baseUrl/series/${manga.url}"
+
+    override fun mangaDetailsRequest(manga: SManga): Request = GET("$apiUrl/series/${manga.url}", headers)
+
+    override fun mangaDetailsParse(response: Response): SManga = response.parseAs<Manga>().toSManga()
+
+    // =============================== Pages ===============================
+
+    override fun pageListRequest(chapter: SChapter): Request = GET("$apiUrl/${chapter.url}")
 
     override fun pageListParse(response: Response): List<Page> {
-        TODO("Not yet implemented")
+        val pages = response.parseAs<Images>()
+        return pages.images.mapIndexed { index, dto -> Page(index, imageUrl = dto.url) }
     }
 
+    // =============================== Popular ===============================
     override fun popularMangaRequest(page: Int): Request {
         val url = "$apiUrl/series".toHttpUrl().newBuilder().apply {
             addQueryParameter("page", page.toString())
@@ -67,6 +105,14 @@ class QiScans : HttpSource() {
             addQueryParameter("sort", "popular")
         }.build()
         return GET(url, headers)
+    }
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val res = response.parseAs<SearchResponse>()
+        var page = res.currentPage
+        var hasNext = !res.nextPage!!.equals(null)
+        val entries = res.data.filterNot { it.type == "NOVEL" }.map { it.toSManga() }
+        return MangasPage(entries, hasNext)
     }
 
     override fun searchMangaParse(response: Response): MangasPage {
@@ -79,14 +125,6 @@ class QiScans : HttpSource() {
         filters: FilterList,
     ): Request {
         TODO("Not yet implemented")
-    }
-
-    override fun popularMangaParse(response: Response): MangasPage {
-        val res = response.parseAs<SearchResponse>()
-        var page = res.currentPage
-        var hasNext = !res.nextPage.equals(null)
-        val entries = res.data.filterNot { it.type == "NOVEL" }.map { it.toSManga() }
-        return MangasPage(entries, hasNext)
     }
 }
 
